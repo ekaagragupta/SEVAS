85b08af5fabfd079312f0d896e9e7bbdb8225507
ekaagra gupta <ekaagrag2006@gmail.com>
2026-01-29 11:10:29 +0530

    lol part new

--- ml-services/utils/image_processor.py
+++ ml-services/utils/image_processor.py
@@ -28,3 +28,59 @@ class ImageProcessor:
             except Exception as e:
                 print("Error loading image:", e)
                 return None
+    
+    def resize_image(self,img_array):
+        
+            try:
+                #current dimensions
+                current_height, current_width = img_array.shape[:2]
+
+                if current_width>self.target_size or current_height>self.target_size:
+                     #shrink image
+                     interpolation = cv2.INTER_AREA
+                else:
+                     #enlarge image
+                     interpolation = cv2.INTER_CUBIC
+                
+                resized= cv2.resize(
+                     img_array,
+                        (self.target_size, self.target_size), 
+                        # interpolation method based on shrinking or enlarging  
+                        interpolation=interpolation
+                )
+                print("image resized to:{self.target_size}*{self.target_size}")
+                return resized
+            except Exception as e:
+                print("Error resizing image:", e)
+                return None
+    
+    def normalize_image(self,img_array):
+            #ml training is better with small no (0-1)
+            try:
+                img_float=img_array.astype(np.float32)
+                normalized=img_float/255.0
+                print("Image normalized.")
+                return normalized
+            except Exception as e:
+                print("Error normalizing image:", e)
+                return None
+    def preprocess_image(self,image_path):
+            #full pipeline 
+            '''
+            1. load 2. resize 3. normalize
+            '''
+            print('starting preprocessing for image:', image_path)
+            img=self.load_image(image_path)
+
+            if img is None:
+                return None
+            
+            img_resized=self.resize_image(img)
+            if img_resized is None:
+                return None
+            
+            print('preprocessing completed')
+            print(f"final shape:{img_resized.shape}")
+            img_normalized=self.normalize_image(img_resized)
+            print(f"value ranges from :[{img_normalized.min():.3f}]")
+            return img_normalized
